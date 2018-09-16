a5abba1611eb6a488d8f53ee3537e931432d510a
iHaq <haqgamer66@gmail.com>
2018-09-16 15:38:44 -0400

    improved videos

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,3 +2,11 @@ document.getElementById('video-2').onload = function () {
     document.getElementById("loader").style.display = "none";
     document.getElementById("right-content").style.display = "block";
 };
+
+function html(image, frameID) {
+    const frame = document.getElementById(frameID);
+
+    image.style.display = "none";
+    frame.style.display = "inline-block";
+    frame.src += "?autoplay=1";
+}
