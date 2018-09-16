f20d02bd976dd6ad55b527ef3b5817f4aa89e491
iHaq <haqgamer66@gmail.com>
2018-09-16 14:11:04 -0400

    improved footer & animations

---
+++ assets/js/global.js
@@ -0,0 +1,4 @@
+document.getElementById('video-2').onload = function () {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("right-content").style.display = "block";
+};
