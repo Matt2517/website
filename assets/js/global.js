b56a7eacdf11edccadaac6da715ec53c7192de18
iHaq <haqgamer66@gmail.com>
2018-09-20 21:01:08 -0400

    fixed videos design

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,9 +3,11 @@ document.getElementById('video-2').onload = function () {
     document.getElementById("right-content").style.display = "block";
 };
 
-function playVideo(image, frameID) {
+function playVideo(span, frameID, imageID) {
     const frame = document.getElementById(frameID);
+    const image = document.getElementById(imageID);
 
+    span.style.display = "none";
     image.style.display = "none";
     frame.style.display = "inline-block";
     frame.src += "?autoplay=1";
